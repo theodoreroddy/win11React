7185a99357ccb08bdbabffdcdf9cdb112b241c08
Theodore Roddy <me@tedroddy.net>
2022-04-03 17:37:25 -0600

    cast version as string

--- src/SystemInformation.js
+++ src/SystemInformation.js
@@ -2,7 +2,7 @@ import env from "react-dotenv"
 
 const SystemInformation = {
     "os": "Windows 11 React",
-    "version": env.OS_VERSION,
+    "version": env.OS_VERSION.toString(),
     "platform": "js",
     "language": "en_US",
     "deployment_env": env.DEPLOYMENT_ENV
