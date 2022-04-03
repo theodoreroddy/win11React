e85d2a644d383c4ca50473a1f4877997039ae449
Theodore Roddy <me@tedroddy.net>
2022-04-03 17:33:53 -0600

    update version bsod

--- src/containers/background/WelcomeMessage.js
+++ src/containers/background/WelcomeMessage.js
@@ -4,7 +4,7 @@ import SystemInformation from '../../SystemInformation';
 
 // Pass the flags prop and return an element based on the state of the feature flag key
 const WelcomeMessage = ({ flags }) => {
-    if (flags['bloatware-removal'] && SystemInformation.version == "11.0") {
+    if (flags['bloatware-removal'] && SystemInformation.version == "11.1") {
         return this.bsod()
     } else {
         return flags['lock-screen-welcome-message'] ? <div>{ flags['lock-screen-welcome-message'].replace("%name%", UserProfile.firstName) }</div> : <div></div>;
